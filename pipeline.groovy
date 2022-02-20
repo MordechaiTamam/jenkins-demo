4ddfe1df64494ce226554efea10e9b86b316f524
MordechaiTamam <modi.tamam@gmail.com>
2022-02-20 20:50:14 +0200

    reuseNode true

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,10 @@ pipeline {
     agent { label 'agent_1' }
     stages {
         stage('list files') {
-            agent { dockerfile true }
+            agent {
+                dockerfile true
+                reuseNode true
+            }
             steps {
                 sh 'python3.9 selenium_simple.py'
             }
