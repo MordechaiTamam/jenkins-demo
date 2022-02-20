3493efb182a8e5360a7cfbf5604c35560843d5ad
MordechaiTamam <modi.tamam@gmail.com>
2022-02-20 19:38:45 +0200

    Minor changes

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,12 +2,7 @@ pipeline {
     agent { label 'agent_1' }
     stages {
         stage('list files') {
-            agent {
-                docker{
-                    dockerfile true
-                    reuseNode true
-                }
-            }
+            agent { dockerfile true }
             steps {
                 sh 'python3.9 selenium_simple.py'
             }
