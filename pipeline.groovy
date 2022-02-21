7bb37b319728aba84f184e5574649336f165d8ad
MordechaiTamam <modi.tamam@gmail.com>
2022-02-21 15:09:45 +0200

    Added stage for building the image

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,17 @@
 pipeline {
     agent { label 'agent_1' }
     stages {
+        stage('Build docker image'){
+            steps{
+                sh 'docker build -t moditamam/selenium:from-jenkins-pipeline .'
+                sh 'docker push moditamam/selenium:from-jenkins-pipeline'
+                sh 'docker rmi moditamam/selenium:from-jenkins-pipeline'
+            }
+        }
         stage('list files') {
             agent {
                 docker {
-                    image "moditamam/selenium:python3"
+                    image "moditamam/selenium:from-jenkins-pipeline"
                     reuseNode true
                 }
             }
